244ff255ef82050f42d48edb487a88f40273ea82
rakshil14-2 <rhmodi@asu.edu>
2024-11-28 00:18:15 -0700

    added details Page

--- habitat-finder/src/App.js
+++ habitat-finder/src/App.js
@@ -1,9 +1,10 @@
+import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
 import './App.css';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 // Import pages
-import ResultsPage from './pages/ResultsPage';
-import HomePage from './pages/HomePage';
 import React from 'react';
+import { DetailsPage } from './pages/DetailsPage';
+import HomePage from './pages/HomePage';
+import ResultsPage from './pages/ResultsPage';
 
 const App= ()=> {
   return (
@@ -16,6 +17,7 @@ const App= ()=> {
         <Routes>
           <Route path="/" element={<HomePage />} />
           <Route path="/results" element={<ResultsPage />} />
+          <Route path="/details" element={<DetailsPage/>}/>
         </Routes>
        
       </main>
---
+++ habitat-finder/src/pages/DetailsPage.js
@@ -0,0 +1,71 @@
+
+import React from "react";
+import Card from 'react-bootstrap/Card';
+import Header from "../components/Header";
+
+export const DetailsPage = () => {
+
+    const defaultMetrics = [
+        { name: "Heat Index", value: 0 },
+        { name: "AQI", value: 0 },
+        { name: "Precipitation Index", value: 0 },
+        { name: "UV Index", value: 0 },
+        { name: "# of Critical Crime", value: 0 },
+        { name: "# of Serious Crime", value: 0 },
+        { name: "# of Moderate Crime", value: 0 }
+      ];
+
+      const getMetricsFromLocalStorage = () => {
+        const storedMetrics = localStorage.getItem('Metrics');//Remember to store that value in localstorage
+        return storedMetrics ? JSON.parse(storedMetrics) : [];
+      };
+    
+      const providedMetrics = getMetricsFromLocalStorage();
+      const metrics = defaultMetrics.map(dm=>{
+        const pm = providedMetrics.find(m=>m.name===dm.name);
+        return pm || dm
+      });
+
+      const environmentMetrics = metrics.filter(m => ["Heat Index", "AQI", "Precipitation Index", "UV Index"].includes(m.name));
+      const crimeMetrics = metrics.filter(m=>["# of Critical Crime", "# of Serious Crime", "# of Moderate Crime"].includes(m.name));
+
+  return (
+    <div className="App">
+      <div className="box">
+        <Header />
+        <div className="detials-content">
+          <h1 className="text-3xl font-bold text-center mb-4">STATISTICS(Recent Data from 2021-2022) of {providedMetrics[0].county} County</h1>
+          <div className="metrics-container">
+            <div className="section">
+              <h2 className="text-2xl font-bold text-center mb-4">Environment Metrics</h2>
+              <div className="metric-placement">
+                {environmentMetrics.map((metric,index)=>(
+                    <Card key={index} className="card" >
+                        <Card.Body>
+                            <Card.Title>{metric.name}</Card.Title>
+                            <Card.Text>{metric.value}</Card.Text>
+                        </Card.Body>
+                    </Card>
+                ))}
+              </div>
+            </div>
+
+            <div className="section">
+              <h2 className="text-2xl font-bold text-center mb-4">Crime Metrics</h2>
+              <div className="metric-placement">
+              {crimeMetrics.map((metric,index)=>(
+                    <Card key={index} className="card" >
+                        <Card.Body>
+                            <Card.Title>{metric.name}</Card.Title>
+                            <Card.Text>{metric.value}</Card.Text>
+                        </Card.Body>
+                    </Card>
+                ))}
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
